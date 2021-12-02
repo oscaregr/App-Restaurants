149400daed290dbe898cf9327b09ba0191d15471
joseLuisMtzE <jmartinez50@ucol.mx>
2021-12-01 21:44:30 -0600

    Validacion e implementacion de iud al crear un restaurante

--- screens/FootupScreen.js
+++ screens/FootupScreen.js
@@ -1,112 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { StatusBar } from "expo-status-bar";
+import React, { useContext } from "react";
 
-import { useState } from 'react';
-import { StyleSheet, Text, View, Button as RNButton } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, Button as RNButton } from "react-native";
 
-import { Button, InputField, ErrorMessage } from '../components';
-import Firebase from '../config/firebase';
+import { Button, InputField, ErrorMessage } from "../components";
+import Firebase from "../config/firebase";
+import { AuthenticatedUserContext } from "../navigation/AuthenticatedUserProvider";
 
 const auth = Firebase.auth();
 
 export default function FootupScreen({ navigation }) {
-  const [restaurant, setRestaurant] = useState('');
-  const [direccion, setDireccion] = useState('');
-  const [rfc, setRFC] = useState('');
-  const [tipo, setTipo] = useState('');
+  const [restaurant, setRestaurant] = useState("");
+  const [direccion, setDireccion] = useState("");
+  const [rfc, setRFC] = useState("");
+  const [tipo, setTipo] = useState("");
+  const { user, setUser } = useContext(AuthenticatedUserContext);
 
   const saveNewRestaurant = () => {
-
-    Firebase.firestore().collection('Restaurantes').add({
-      direccion: direccion,
-      nombre: restaurant,
-      rfc: rfc,
-      tipo: tipo
-    })
-    
-  }
+    if (
+      direccion.length > 0 &&
+      restaurant.length > 0 &&
+      rfc.length > 0 &&
+      tipo.length > 0 &&
+      user.uid.length > 0
+    ) {
+      Firebase.firestore().collection("Restaurantes").add({
+        direccion: direccion,
+        nombre: restaurant,
+        rfc: rfc,
+        tipo: tipo,
+        uid: user.uid,
+      });
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <StatusBar style='dark-content' />
+      <StatusBar style="dark-content" />
       <Text style={styles.title}>Rigistrar Restaurante</Text>
       <InputField
         inputStyle={{
-          fontSize: 14
+          fontSize: 14,
         }}
         containerStyle={{
-          backgroundColor: '#fff',
-          marginBottom: 20
+          backgroundColor: "#fff",
+          marginBottom: 20,
         }}
-        leftIcon='store'
-        placeholder='Nombre del restaurante'
-        autoCapitalize='none'
+        leftIcon="store"
+        placeholder="Nombre del restaurante"
+        autoCapitalize="none"
         autoFocus={true}
         value={restaurant}
-        onChangeText={text => setRestaurant(text)}
+        onChangeText={(text) => setRestaurant(text)}
       />
       <InputField
         inputStyle={{
-          fontSize: 14
+          fontSize: 14,
         }}
         containerStyle={{
-          backgroundColor: '#fff',
-          marginBottom: 20
+          backgroundColor: "#fff",
+          marginBottom: 20,
         }}
-        leftIcon='map-marker'
-        placeholder='Direccion'
-        autoCapitalize='none'
+        leftIcon="map-marker"
+        placeholder="Direccion"
+        autoCapitalize="none"
         autoCorrect={true}
         value={direccion}
-        onChangeText={text => setDireccion(text)}
-      />
-       
-       <InputField
-        inputStyle={{
-          fontSize: 14
-        }}
-        containerStyle={{
-          backgroundColor: '#fff',
-          marginBottom: 20
-        }}
-        leftIcon='file-document'
-        placeholder='RFC'
-        autoCapitalize='none'
-        autoCorrect={true}
-        value={rfc}
-        onChangeText={text => setRFC(text)}
+        onChangeText={(text) => setDireccion(text)}
       />
 
-        <InputField
+      <InputField
         inputStyle={{
-          fontSize: 14
+          fontSize: 14,
         }}
         containerStyle={{
-          backgroundColor: '#fff',
-          marginBottom: 20
+          backgroundColor: "#fff",
+          marginBottom: 20,
         }}
-        leftIcon='typewriter'
-        placeholder='Tipo'
-        autoCapitalize='none'
+        leftIcon="file-document"
+        placeholder="RFC"
+        autoCapitalize="none"
+        autoCorrect={true}
+        value={rfc}
+        onChangeText={(text) => setRFC(text)}
+      />
+
+      <InputField
+        inputStyle={{
+          fontSize: 14,
+        }}
+        containerStyle={{
+          backgroundColor: "#fff",
+          marginBottom: 20,
+        }}
+        leftIcon="typewriter"
+        placeholder="Tipo"
+        autoCapitalize="none"
         autoCorrect={true}
         value={tipo}
-        onChangeText={text => setTipo(text)}
+        onChangeText={(text) => setTipo(text)}
       />
 
       <Button
         onPress={saveNewRestaurant}
-        backgroundColor='#467fd0'
-        title='Crear'
-        tileColor='#fff'
+        backgroundColor="#467fd0"
+        title="Crear"
+        tileColor="#fff"
         titleSize={20}
         containerStyle={{
-          marginBottom: 24
+          marginBottom: 24,
         }}
       />
       <RNButton
-        onPress={() => navigation.navigate('Home')}
-        title='Volver'
-        color='#fd9644'
+        onPress={() => navigation.navigate("Home")}
+        title="Volver"
+        color="#fd9644"
       />
     </View>
   );
@@ -115,15 +124,15 @@ export default function FootupScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#c83609',
+    backgroundColor: "#c83609",
     paddingTop: 50,
-    paddingHorizontal: 12
+    paddingHorizontal: 12,
   },
   title: {
     fontSize: 24,
-    fontWeight: '600',
-    color: '#fff',
-    alignSelf: 'center',
-    paddingBottom: 24
-  }
+    fontWeight: "600",
+    color: "#fff",
+    alignSelf: "center",
+    paddingBottom: 24,
+  },
 });
